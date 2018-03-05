f7cd7d7d3ea14f369c26a5dc7217846e1d9fcf19
Kevin Kirn <kevin.kirn@students.fhnw.ch>
2018-03-05 16:18:00 +0100

    adds initial backend spring boot project

---
+++ backend/src/main/java/ch/fhnw/wodss/tournament/TournamentApplication.java
@@ -0,0 +1,12 @@
+package ch.fhnw.wodss.tournament;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TournamentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TournamentApplication.class, args);
+	}
+}
