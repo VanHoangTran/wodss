749474e1b46e0db35293a9f9ff5574bb33dd0946
Kevin Kirn <kevin.kirn@primesystems.ch>
2018-03-20 08:59:39 +0100

    replaces standard react with a flux/react boilerplate

--- frontend/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import Login from './Login';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<Login />, document.getElementById('root'));
-registerServiceWorker();
