edb0d570105fe7e951ca28ee0d212d1fb8bf4c4b
Hoang Tran <hoang.tran1993@gmail.com>
2018-05-11 08:59:10 +0200

    added Header

--- frontend/src/components/header/Header.js
+++ frontend/src/components/header/Header.js
@@ -1,8 +1,11 @@
-import React from 'react'
+import React, {Component} from 'react'
 import {NavLink} from 'react-router-dom'
 import FlatButton from 'material-ui/FlatButton';
 import {Tab, Tabs} from 'material-ui/Tabs';
 import {colors, dimensions} from "../../util/constants";
+import MatchList from "../match-list/MatchList";
+import SignUp from "../sign-up/SignUp";
+import {Route, Switch} from 'react-router-dom';
 
 const styles = {
     toolbar: {
@@ -25,32 +28,51 @@ const styles = {
     },
     tab: {
         height: dimensions.toolbar,
+    },
+    avatarButton: {
+        height: '100%',
+        float: 'right',
+    },
+    avatar: {
+        height: '2rem',
+        margin: '1rem',
+        borderRadius: '1.5rem',
     }
 };
 
-const Header = () => (
-    <div className="header">
-        <NavLink exact to="/">Startseite</NavLink>
-        <NavLink exact to="/logout">Logout</NavLink>
-        <div style={styles.toolbar}>
-            <div style={styles.logoContainer}>
-                LOGO
-            </div>
-            <Tabs style={styles.tabs} inkBarStyle={styles.tabIndicator}>
-                <Tab label="Tippspiel" style={styles.tab}/>
-                <Tab label="Tippteam" style={styles.tab}/>
-                <Tab
-                    label="Admin"
-                    data-route="/Startseite"
-                    style={styles.tab}
-                />
-            </Tabs>
-            <FlatButton rippleColor={colors.light} hoverColor="transparent">
-                asdfasdf asd
-            </FlatButton>
+const Header = () => {
+    return (
 
+        <div className="header">
+            <Switch>
+                <Route exact path="/" component={MatchList}/>
+                <Route exact path="/signUp" component={SignUp}/>
+            </Switch>
+            <NavLink exact to="/">Startseite</NavLink>
+            <NavLink exact to="/logout">Logout</NavLink>
+            <div style={styles.toolbar}>
+                <div style={styles.logoContainer}>
+                    LOGO
+                    <NavLink exact to="/signUp">Test</NavLink>
+                </div>
+                <Tabs style={styles.tabs} inkBarStyle={styles.tabIndicator}>
+                    <Tab label="Tippspiel" style={styles.tab}/>
+                    <Tab label="Tippteam" style={styles.tab}/>
+                    <Tab
+                        label="Admin"
+                        data-route="/Startseite"
+                        style={styles.tab}
+                    />
+                    <NavLink exact to="/logout">Test</NavLink>
+                </Tabs>
+                <FlatButton rippleColor={colors.light} hoverColor="transparent" style={styles.avatarButton}>
+                    <img id="avatar" style={styles.avatar} alt=""
+                         src="https://api.adorable.io/avatars/100/HoangTran.png"/>
+                </FlatButton>
+            </div>
         </div>
-    </div>
-);
+    )
+};
+
 
 export default Header
