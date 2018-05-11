903a00d4b9c7e17e87a0de3acc1723f3c46a81f5
Hoang Tran <hoang.tran1993@gmail.com>
2018-05-11 12:59:38 +0200

    readded deleted strings

--- frontend/src/strings.js
+++ frontend/src/strings.js
@@ -2,6 +2,8 @@ import LocalizedStrings from 'react-localization';
 
 export const strings = new LocalizedStrings({
     de: {
+        ok: "OK",
+
         login: "Login",
         username: "Benutzername",
         password: "Passwort",
@@ -12,6 +14,12 @@ export const strings = new LocalizedStrings({
         noAccountYet: "Noch keinen Account?",
         signUp: "Registrieren",
         mail: "E-Mail-Adresse",
+        accountVerified: "Account verifiziert",
+        accountVerifiedSuccessfully: "Dein Account wurde erfolgreich verifiziert.",
+
+        profile: "Profil",
+        userInformation: "Benutzerinformationen",
+        changePassword: "Passwort ändern",
 
         goals: "Tore",
     }
