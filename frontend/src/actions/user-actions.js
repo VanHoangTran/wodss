4a02902ed8b9e08ccb09833363da4d169bb603ce
Hoang Tran <hoang.tran1993@gmail.com>
2018-05-14 18:23:33 +0200

    implementing edit profile

--- frontend/src/actions/user-actions.js
+++ frontend/src/actions/user-actions.js
@@ -1,17 +1,18 @@
 import $ from 'jquery';
-import {API_ACTION_AUTH, API_ENDPOINT} from '../util/constants';
+import {API_ACTION_ACCOUNT, API_ACTION_AUTH, API_ENDPOINT} from '../util/constants';
 
 const CONTENT_TYPE = "application/json; charset=utf-8";
 
 export const UPDATE_USER = 'user:updateUser';
 
-export function updateUser(username, token) {
+export function updateUser(username, token, mail) {
     return {
         type: UPDATE_USER,
         payload: {
             user: {
                 username: username,
                 token: token,
+                mail: mail
             }
         }
     }
@@ -26,10 +27,10 @@ export function apiAuthenticate(username, password) {
             data: JSON.stringify({username: username, password: password}),
             contentType: CONTENT_TYPE,
             success(response) {
-                dispatch(updateUser(username, response.id_token));
+                dispatch(updateUser(username, response.id_token, null));
             },
             error(response) {
-                dispatch(updateUser(null, null));
+                dispatch(updateUser(null, null, null));
             }
         })
     }
@@ -37,6 +38,22 @@ export function apiAuthenticate(username, password) {
 
 export function apiLogout() {
     return dispatch => {
-        dispatch(updateUser(null, null));
+        dispatch(updateUser(null, null, null));
+    }
+}
+
+export function apiGetAccountInformation() {
+    return dispatch => {
+        $.ajax({
+            type: 'GET',
+            url: API_ENDPOINT + API_ACTION_ACCOUNT,
+            contentType: CONTENT_TYPE,
+            success(response) {
+                console.log(response);
+            },
+            error(response) {
+                console.log(response);
+            }
+        })
     }
 }
