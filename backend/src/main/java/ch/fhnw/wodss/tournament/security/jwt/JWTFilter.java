8bebea2649dac0abc83dfc20fe3a3cfd12305fd9
Kevin Kirn <kevin.kirn@students.fhnw.ch>
2018-05-22 11:20:52 +0200

    limits acces with jwt token to 2500 requests / 5 minutes

--- backend/src/main/java/ch/fhnw/wodss/tournament/security/jwt/JWTFilter.java
+++ backend/src/main/java/ch/fhnw/wodss/tournament/security/jwt/JWTFilter.java
@@ -1,6 +1,8 @@
 package ch.fhnw.wodss.tournament.security.jwt;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
@@ -15,10 +17,16 @@ import org.springframework.web.filter.GenericFilterBean;
 
 public class JWTFilter extends GenericFilterBean {
 
+	private static final int MAX_REQUESTS_PER_MINUTE = 500;
+	private static final int MINUTES_REQUEST_LIMIT = 5;
+
 	private final TokenProvider tokenProvider;
+
+	private final Map<String, long[]> requestStats;
 
 	public JWTFilter(TokenProvider tokenProvider) {
 		this.tokenProvider = tokenProvider;
+		this.requestStats = new HashMap<>();
 	}
 
 	@Override
@@ -26,11 +34,41 @@ public class JWTFilter extends GenericFilterBean {
 			throws IOException, ServletException {
 		HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
 		String jwt = resolveToken(httpServletRequest);
-		if (StringUtils.hasText(jwt) && this.tokenProvider.validateToken(jwt)) {
+
+		// limit access per token
+		boolean canPassThrough = validateTokenRequestLimit(jwt);
+
+		if (canPassThrough && StringUtils.hasText(jwt) && this.tokenProvider.validateToken(jwt)) {
 			Authentication authentication = this.tokenProvider.getAuthentication(jwt);
 			SecurityContextHolder.getContext().setAuthentication(authentication);
 		}
 		filterChain.doFilter(servletRequest, servletResponse);
+	}
+
+	private boolean validateTokenRequestLimit(String jwt) {
+		if (jwt != null && requestStats.containsKey(jwt)) {
+			long[] stats = requestStats.get(jwt);
+			long counter = stats[0]; // how many requests were yet send
+			long firstRequest = stats[1]; // date in milliseconds of last request
+			stats[0] = ++counter; // increase request counter
+
+			// calculate diff between last and current request
+			long currentTime = System.currentTimeMillis();
+			long diff = currentTime - firstRequest;
+
+			if (diff < 60000 * MINUTES_REQUEST_LIMIT) {
+				if (counter > MAX_REQUESTS_PER_MINUTE * MINUTES_REQUEST_LIMIT) {
+					return false;
+				}
+			} else {
+				// reset limits
+				stats[0] = 1;
+				stats[1] = System.currentTimeMillis();
+			}
+		} else if (jwt != null) {
+			requestStats.put(jwt, new long[] { 1, System.currentTimeMillis() });
+		}
+		return true;
 	}
 
 	private String resolveToken(HttpServletRequest request) {
