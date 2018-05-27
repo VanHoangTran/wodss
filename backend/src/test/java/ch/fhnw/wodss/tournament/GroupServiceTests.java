4b4db69b414a9f96638d1f36162762b4c5f4a0b7
Hoang Tran <hoang.tran1993@gmail.com>
2018-05-27 16:35:26 +0200

    adjusted group service tests and phase service tests

--- backend/src/test/java/ch/fhnw/wodss/tournament/GroupServiceTests.java
+++ backend/src/test/java/ch/fhnw/wodss/tournament/GroupServiceTests.java
@@ -1,5 +1,9 @@
 package ch.fhnw.wodss.tournament;
 
+import ch.fhnw.wodss.tournament.domain.TournamentGroup;
+import ch.fhnw.wodss.tournament.repository.TournamentGroupRepository;
+import ch.fhnw.wodss.tournament.service.GroupService;
+import ch.fhnw.wodss.tournament.service.dto.GroupDTO;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -10,52 +14,49 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import ch.fhnw.wodss.tournament.domain.TournamentGroup;
-import ch.fhnw.wodss.tournament.repository.TournamentGroupRepository;
-import ch.fhnw.wodss.tournament.service.GroupService;
-import ch.fhnw.wodss.tournament.service.dto.GroupDTO;
-
 /**
  * Performs test of group service
- * 
+ *
  * @author Kevin Kirn <kevin.kirn@students.fhnw.ch>
  */
 @RunWith(SpringRunner.class)
 public class GroupServiceTests {
 
-	@TestConfiguration
-	static class TestInitalizer {
+    @TestConfiguration
+    static class TestInitializer {
 
-		@Bean
-		public GroupService groupService() {
-			return new GroupService();
-		}
-	}
+        @Bean
+        public GroupService groupService() {
+            return new GroupService();
+        }
+    }
 
-	@Autowired
-	private GroupService groupService;
+    @Autowired
+    private GroupService groupService;
 
-	@MockBean
-	private TournamentGroupRepository groupRepository;
+    @MockBean
+    private TournamentGroupRepository groupRepository;
 
-	@Test
-	public void testGetGroupByName() {
-		// invalid group name provided
-		Mockito.when(groupRepository.findByName(Mockito.anyString())).thenReturn(null);
-		try {
-			groupService.getGroupByName("group which does not exists ;-)");
-			Assert.fail("should never reach here...");
-		} catch (Exception e) {
-		}
+    @Test
+    public void testGetGroupByName_invalid() {
+        // invalid group name provided
+        Mockito.when(groupRepository.findByName(Mockito.anyString())).thenReturn(null);
+        try {
+            groupService.getGroupByName("group which does not exists ;-)");
+            Assert.fail("getGroupByName did not fail with invalid group name");
+        } catch (Exception ignored) {
+        }
+    }
 
-		// good case
-		TournamentGroup mock = new TournamentGroup();
-		mock.setId(99L);
-		mock.setName("Hall of Fame");
-		Mockito.when(groupRepository.findByName(mock.getName())).thenReturn(mock);
+    @Test
+    public void testGetGroupByName() {
+        TournamentGroup mock = new TournamentGroup();
+        mock.setId(99L);
+        mock.setName("Hall of Fame");
+        Mockito.when(groupRepository.findByName(mock.getName())).thenReturn(mock);
 
-		GroupDTO dto = groupService.getGroupByName(mock.getName());
-		Assert.assertEquals(mock.getId(), dto.getId());
-		Assert.assertEquals(mock.getName(), dto.getName());
-	}
+        GroupDTO dto = groupService.getGroupByName(mock.getName());
+        Assert.assertEquals(mock.getId(), dto.getId());
+        Assert.assertEquals(mock.getName(), dto.getName());
+    }
 }
